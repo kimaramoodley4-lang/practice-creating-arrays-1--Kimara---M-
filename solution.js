b4fcd845759f46626125540a803dfdd3a77f88b7
kimaramoodley4-lang <kimaramoodley4@gmail.com>
2026-04-22 12:54:18 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,19 @@
+// Task 1: Create Arrays
+let groceryItems = ["Milk", "Bread", "Eggs", "Apples", "Cheese"];
+let prices = [15.50, 22.00, 45.00];
+
+// Task 2: Access and Log the Values
+// 1. Log the entire array of grocery items
+console.log(groceryItems);
+
+// 2. Log the entire array of prices
+console.log(prices);
+
+// 3. Log three individual items (using index 0, 1, and 2)
+console.log(groceryItems[0]);
+console.log(groceryItems[1]);
+console.log(groceryItems[2]);
+
+// 4. Log two prices (using index 0 and 1)
+console.log(prices[0]);
+console.log(prices[1]);
